7c58ff43a4963636cfc5710fb89c620aacf862f3
Vishal0746 <vp0072003@gmail.com>
2022-09-27 11:35:57 +0530

    first commit

---
+++ weather.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+const https = require("https")
+const bodyparser = require("body-parser")
+app.use(bodyparser.urlencoded({extented :true}));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+app.get("/", function(req,res)
+{
+  res.render("index", { weather: null, error: null });
+
+})
+
+app.post("/", function(req,res){
+const city =req.body.city;
+
+const url ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d9d8a843772e81e09b4e1615e47004ce&units=metric&modle";
+https.get(url, function(response)
+{
+//console.log(response.statusCode);
+
+response.on("data", function(data)
+{
+  //printing data from api
+
+  const weather =JSON.parse(data);
+  console.log(data);
+
+  let place = `${weather.name}`,
+                    /* you shall calculate the current timezone using the data fetched*/
+                    weatherTimezone = `${new Date(
+                      weather.dt * 1000 - weather.timezone * 1000
+                    )}`;
+                  let weatherTemp = `${weather.main.temp}`,
+                    weatherPressure = `${weather.main.pressure}`,
+                    /* you will fetch the weather icon and its size using the icon data*/
+                    weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`,
+                    weatherDescription = `${weather.weather[0].description}`,
+                    humidity = `${weather.main.humidity}`,
+                    clouds = `${weather.clouds.all}`,
+                    visibility = `${weather.visibility}`,
+                    main = `${weather.weather[0].main}`,
+                    weatherFahrenheit;
+                  weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+
+
+                  res.render("index", {
+                                    weather: weather,
+                                    place: place,
+                                    temp: weatherTemp,
+                                    pressure: weatherPressure,
+                                    icon: weatherIcon,
+                                    description: weatherDescription,
+                                    timezone: weatherTimezone,
+                                    humidity: humidity,
+                                    fahrenheit: weatherFahrenheit,
+                                    clouds: clouds,
+                                    visibility1: visibility,
+                                    main: main,
+                                    error: null,
+                                  });
+                                  console.log("timeone :- "+weatherDescription);
+
+})
+})
+
+})
+
+
+app.listen(port, () =>
+{
+console.log("ready");
+})
